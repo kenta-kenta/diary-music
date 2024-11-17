d3f2a597dfc4a0f47fd836457639331dcdcc7681
kenta-kenta <kenken22352235@gmail.com>
2024-11-17 20:13:06 +0900

    フッターをページの最後になるように変更しました

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-orange-50 text-orange-600 p-4 text-center shadow-md">
+    <footer className="bottom-0 w-full bg-orange-50 text-orange-600 p-4 text-center shadow-md">
       <div className="flex justify-center space-x-8 pb-2">
         <Link to="/diary" className="hover:text-orange-800 transition-colors">
           日記を書く
